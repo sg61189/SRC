33c595e75c62f92ada9c5bfcf60600f46d5d064a
Devleena Ghosh <devleena@Devleenas-MacBook-Air.local>
2023-05-24 10:35:27 +0530

    Updates-11.2

--- codes/src/run_abc5.sh
+++ codes/src/run_abc5.sh
@@ -6,18 +6,9 @@ while read line; do
 #Reading each line.
 echo "----------------- ${i} Running file ${line} ------------------"
 fn=${line}
-echo "-----------------  bmc3g ------------------"
-../ABC/abc -c "read ../benchmark/HWMCC_15_17/${fn}.aig; print_stats; &get; &dc2;&put;dretime;&get;&lcorr;&dc2;&put;dretime;&get;&scorr;&fraig;&dc2;&put;dretime;&put; print_stats; &get; bmc3 -g -S 0 -T ${to} -v -L stdout; print_stats;" > logs_IF/log_abc_bmc3g_${fn}.txt
 
-echo "-----------------  bmc2 ------------------"
-../ABC/abc -c "read ../benchmark/HWMCC_15_17/${fn}.aig; print_stats; &get; &dc2;&put;dretime;&get;&lcorr;&dc2;&put;dretime;&get;&scorr;&fraig;&dc2;&put;dretime;&put; print_stats; &get; bmc2 -S 0 -T ${to} -v -L stdout; print_stats;" > logs_IF/log_abc_bmc2_${fn}.txt
-
-echo "-----------------  bmc3 ------------------"
-../ABC/abc -c "read ../benchmark/HWMCC_15_17/${fn}.aig; print_stats; &get; &dc2;&put;dretime;&get;&lcorr;&dc2;&put;dretime;&get;&scorr;&fraig;&dc2;&put;dretime;&put; print_stats; &get; bmc3 -S 0 -T ${to} -v -L stdout; print_stats;" > logs_IF/log_abc_bmc3_${fn}.txt
-
-echo "-----------------  bmc3s ------------------"
-../ABC/abc -c "read ../benchmark/HWMCC_15_17/${fn}.aig; print_stats; &get; &dc2;&put;dretime;&get;&lcorr;&dc2;&put;dretime;&get;&scorr;&fraig;&dc2;&put;dretime;&put; print_stats; &get; bmc3s -S 0 -T ${to} -v -L stdout; print_stats;" > logs_IF/log_abc_bmc3s_${fn}.txt
-
+echo "----------------- Separate abc ------------------"
+python3 run_abc.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_IF/log_abc_${fn}.txt
 #fi
 #break
 i=$((i+1))
