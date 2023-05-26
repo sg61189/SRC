295a448c80328358fc204204def12640337818bc
Devleena Ghosh <devleena@Devleenas-MacBook-Air.local>
2023-05-26 14:04:19 +0530

    Updates-14.1

--- codes/src/runMAB_selected.sh
+++ codes/src/runMAB_selected.sh
@@ -2,7 +2,15 @@
 to=3600
 i=1
 fn=6s13
-echo "----------------- Running MAB_bmc_fixed time ------------------"
+echo "----------------- Running MAB_bmc_fixed time ${fn} ------------------"
+python3 MAB_BMC_fixt_ucb1.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_IF/log_mabmc_fix_ucb1_IF_${fn}_D3.txt
+
+fn=6s309b034
+echo "----------------- Running MAB_bmc_fixed time ${fn} ------------------"
+python3 MAB_BMC_fixt_ucb1.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_IF/log_mabmc_fix_ucb1_IF_${fn}_D3.txt
+
+fn=6s320rb0
+echo "----------------- Running MAB_bmc_fixed time ${fn} ------------------"
 python3 MAB_BMC_fixt_ucb1.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_IF/log_mabmc_fix_ucb1_IF_${fn}_D3.txt
 
 #break
