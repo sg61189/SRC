99168364f7efd194a21f3e2c86cd2f804b356de7
Devleena Ghosh <devleena@Devleenas-MacBook-Air.local>
2023-07-25 20:02:45 +0530

    code changes 2.2_MC

--- codes/src/runMABn1_6s.sh
+++ codes/src/runMABn1_6s.sh
@@ -7,16 +7,15 @@ while read line; do
 echo "----------------- ${i} Running file ${line} ------------------"
 fn=${line}
 echo "----------------- Running MAB_bmc_fixed time ------------------"
-python3 MAB_BMC_fixt_n.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_MC/log_mabmc_fix_IF_${fn}_D3.txt
+python3 MAB_BMC_fixt_n.py -i benchmark/HWMCC_15_17/${fn}.aig > results_MC/log_mabmc_fix_IF_${fn}_D3.txt
 echo "----------------- Running MAB_bmc_new ------------------"
-python3 MAB_BMC_new_n.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_MC/log_mabmc_IF_${fn}_D3.txt
-
-# echo "----------------- Separate abc ------------------"
-# python3 run_abc.py -i benchmark/HWMCC_15_17/${fn}.aig > logs_IF/log_abc_${fn}.txt
+python3 MAB_BMC_new_n.py -i benchmark/HWMCC_15_17/${fn}.aig > results_MC/log_mabmc_IF_${fn}_D3.txt
+echo "----------------- Separate abc ------------------"
+python3 run_abc.py -i benchmark/HWMCC_15_17/${fn}.aig > results_MC/log_abc_${fn}.txt
 #echo "----------------- Running MAB_bmc_frame ------------------"
 #python3 MAB_BMC_fo.py -i benchmark/HWMCC_15_17/${fn}.aig > logs/log_mabmc_fo_${fn}_D3.txt
-# echo "----------------- Running Superdeep ------------------"
-##(time (timeout ${to} ../super-prove-build/build/super_prove/bin/super_deep.sh -r stdout ../benchmark/HWMCC_15_17/${fn}.aig) 2>&1) > logs/log_deep_${fn}_D2.txt 
+echo "----------------- Running Superdeep ------------------"
+(time (timeout ${to} ../super-prove-build/build/super_prove/bin/super_deep.sh -r stdout ../benchmark/HWMCC_15_17/${fn}.aig) 2>&1) > results_MC/log_deep_${fn}_D2.txt 
 
 #python3 MAB_BMC_frame.py -i benchmark/HWMCC_15_17/${fn}.aig > logs/log_${fn}_D2_frame.txt
 
